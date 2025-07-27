9a852d940462693c5224f8640bc3a68baa0acdc5
strfunctionk <comzun01@gmail.com>
2025-07-27 16:30:44 +0900

    docs: auth 문서 작성

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,100 @@ import { bodyToSignUp, bodyToSignIn, bodyToRefresh } from "../dtos/auth.dto.js";
 import { signUp, signIn, signOut, refresh } from "../services/auth.service.js";
 
 export const handleSignUp = async (req, res, next) => {
+  /*
+    #swagger.tags = ['Auth']
+    #swagger.summary = '회원가입'
+    #swagger.description = '회원가입을 위한 API입니다. 이메일, 이름, 비밀번호를 포함해 요청해야 합니다.'
+    
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              name: { type: 'string', example: '이름' },
+              email: { type: 'string', example: 'email@email.com' },
+              password: { type: 'string', example: '비밀번호' },
+            },
+            required: ['email', 'name', 'password']
+          }
+        }
+      }
+    }
+
+    #swagger.responses[200] = {
+      description: '회원가입 성공',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'SUCCESS' },
+              error: { type: 'object', example: null },
+              success: {
+                type: 'object',
+                properties: {
+                  id: { type: 'number', example: 1 },
+                  name: { type: 'string', example: '이름' },
+                  email: { type: 'string', example: 'email@email.com' },
+                  refreshToken: { type: 'string', example: 'refreshToken' },
+                  password: { type: 'string', example: '비밀번호' },
+                  createdAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' },
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[409] = {
+      description: '이메일 중복',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'duplicate_email' },
+                  reason: { type: 'string', example: '이미 존재하는 이메일입니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[400] = {
+      description: '잘못된 요청',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'invalid_request' },
+                  reason: { type: 'string', example: '요청 데이터가 잘못되었습니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+*/
   try {
     const auth = await signUp(bodyToSignUp(req.body));
     res.status(StatusCodes.OK).success(auth);
@@ -11,6 +105,76 @@ export const handleSignUp = async (req, res, next) => {
   }
 };
 export const handleSignIn = async (req, res, next) => {
+    /*
+    #swagger.tags = ['Auth']
+    #swagger.summary = '로그인'
+    #swagger.description = '로그인을 위한 API입니다. 이메일, 비밀번호를 포함해 요청해야 합니다.'
+
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              email: { type: 'string', example: 'email@email.com' },
+              password: { type: 'string', example: '비밀번호' },
+            },
+            required: ['email', 'password']
+          }
+        }
+      }
+    }
+
+    #swagger.responses[200] = {
+      description: '로그인 성공',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'SUCCESS' },
+              error: { type: 'object', example: null },
+              success: {
+                type: 'object',
+                properties: {
+                  id: { type: 'number', example: 1 },
+                  name: { type: 'string', example: '이름' },
+                  accessToken: { type: 'string', example: 'accessToken' },
+                  refreshToken: { type: 'string', example: 'refreshToken' },
+                  createdAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' },
+                  updatedAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[400] = {
+      description: '잘못된 요청',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'invalid_request' },
+                  reason: { type: 'string', example: '요청 데이터가 잘못되었습니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+*/
   try {
     const auth = await signIn(bodyToSignIn(req.body));
     res.status(StatusCodes.OK).success(auth);
@@ -19,6 +183,54 @@ export const handleSignIn = async (req, res, next) => {
   }
 };
 export const handleSignOut = async (req, res, next) => {
+  /*
+    #swagger.tags = ['Auth']
+    #swagger.summary = '로그아웃'
+    #swagger.description = '로그아웃을 위한 API입니다.'
+
+    #swagger.responses[200] = {
+      description: '로그아웃 성공',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'SUCCESS' },
+              error: { type: 'object', example: null },
+              success: {
+                type: 'object',
+                properties: {
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[400] = {
+      description: '잘못된 요청',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'invalid_request' },
+                  reason: { type: 'string', example: '요청 데이터가 잘못되었습니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+*/
   try {
     if (req.user && req.user.userId) {
       const auth = await signOut(req.user.userId);
@@ -31,6 +243,75 @@ export const handleSignOut = async (req, res, next) => {
   }
 };
 export const handleRefresh = async (req, res, next) => {
+/*
+    #swagger.tags = ['Auth']
+    #swagger.summary = '리프레시 토큰 갱신'
+    #swagger.description = '리프레시 토큰을 갱신하기 위한 API입니다.'
+
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              refreshToken: { type: 'string', example: 'refreshToken' },
+            },
+            required: ['refreshToken']
+          }
+        }
+      }
+    }
+
+    #swagger.responses[200] = {
+      description: '로그인 성공',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'SUCCESS' },
+              error: { type: 'object', example: null },
+              success: {
+                type: 'object',
+                properties: {
+                  id: { type: 'number', example: 1 },
+                  name: { type: 'string', example: '이름' },
+                  accessToken: { type: 'string', example: 'accessToken' },
+                  refreshToken: { type: 'string', example: 'refreshToken' },
+                  createdAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' },
+                  updatedAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[400] = {
+      description: '잘못된 요청',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'invalid_request' },
+                  reason: { type: 'string', example: '요청 데이터가 잘못되었습니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+*/
   try {
     const auth = await refresh(bodyToRefresh(req.body));
     res.status(StatusCodes.OK).success(auth);
@@ -39,6 +320,54 @@ export const handleRefresh = async (req, res, next) => {
   }
 };
 export const handleProtect = async (req, res, next) => {
+    /*
+    #swagger.tags = ['Auth']
+    #swagger.summary = '엑세스 토큰 확인'
+    #swagger.description = '엑세스 토큰을 확인하기 위한 API입니다.'
+
+    #swagger.responses[200] = {
+      description: '엑세스 토큰 확인 성공',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'SUCCESS' },
+              error: { type: 'object', example: null },
+              success: {
+                type: 'object',
+                properties: {
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    #swagger.responses[400] = {
+      description: '잘못된 요청',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              resultType: { type: 'string', example: 'FAIL' },
+              error: {
+                type: 'object',
+                properties: {
+                  errorCode: { type: 'string', example: 'invalid_request' },
+                  reason: { type: 'string', example: '요청 데이터가 잘못되었습니다.' },
+                  data: { type: 'object', example: null }
+                }
+              },
+              success: { type: 'object', example: null }
+            }
+          }
+        }
+      }
+    }
+*/
   try {
     const auth = req.user.userId;
     res.status(StatusCodes.OK).success(auth);
