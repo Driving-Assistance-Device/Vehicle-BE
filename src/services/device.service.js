3d229fbf1e4bb4bf55fae2e019cc339b5ebca4d3
strfunctionk <comzun01@gmail.com>
2025-08-10 15:48:27 +0900

    fix: 에러 수정

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -9,20 +9,23 @@ import { responseFromDevice, responseFromDevices } from "../dtos/device.dto.js";
 export const deviceHello = async (payload) => {
   if (payload && payload.code) {
     const device = await getDeviceByCode(payload.code);
-
-    const data = {
-      id: device.id,
-      status: 0, // 상태를 초기화
-    };
-
     if (device) {
+      const data = {
+        id: device.id,
+        status: 0, // 상태를 초기화
+      };
       if (device.status === 2) {
-        await patchDevice(data);
+        const newDevice = await patchDevice(data);
+        return responseFromDevice({ newDevice });
       }
       return responseFromDevice({ device });
     }
 
     console.log("디바이스 없음 -> 새로 등록");
+    const data = {
+      code: payload.code,
+      status: 0, // 초기 상태는 false로 설정
+    };
     const deviceId = await addDevice(data);
     const newDevice = await getDevice(deviceId);
     return responseFromDevice({ device: newDevice });
